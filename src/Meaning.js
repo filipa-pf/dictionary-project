78339b2d13adba0c629440f29f880a689335778e
fmf4te <anafilipa.paesdefaria@gmail.com>
2022-08-07 16:05:52 +0100

    styled content

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,14 +4,14 @@ export default function Meaning(props) {
     console.log(props.meaning);
     return (
         <div className="Meaning">
-            <h3>{props.meaning.partOfSpeech}</h3>
+            <h3 className="fs-5 fw-bold">{props.meaning.partOfSpeech}</h3>
             {props.meaning.definitions.map(function (definition, index) {
                 return (
                     <div key={index}>
                         <p>
                             {definition.definition}
                         </p>
-                        <p className="small">
+                        <p className="small ps-3">
                             {definition.example}
                         </p>
                     </div>
