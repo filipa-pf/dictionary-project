3df7d500daada1300fd3c08a8ef0b6eefdc1c882
fmf4te <anafilipa.paesdefaria@gmail.com>
2022-08-08 17:38:32 +0100

    added rel="noreferrer"

--- src/Photos.js
+++ src/Photos.js
@@ -9,7 +9,7 @@ export default function Photos(props) {
           {props.photos.map(function (photo, index) {
             return (
               <div className="col-4">
-                <a href={photo.src.original} target="_blank">
+                <a href={photo.src.original} target="_blank" rel="noreferrer">
                 <img
                   src={photo.src.landscape}
                   key={index}
