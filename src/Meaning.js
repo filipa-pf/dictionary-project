2ebf572eae8a0c8dafed1644c9b82d365b56fbc9
fmf4te <anafilipa.paesdefaria@gmail.com>
2022-08-08 12:26:56 +0100

    added examples and synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,22 +1,24 @@
 import React from "react";
+import Synonyms from "./Synonyms";
+
 
 export default function Meaning(props) {
-    console.log(props.meaning);
-    return (
-        <div className="Meaning">
-            <h3 className="fs-5 fw-bold">{props.meaning.partOfSpeech}</h3>
-            {props.meaning.definitions.map(function (definition, index) {
-                return (
-                    <div key={index}>
-                        <p>
-                            {definition.definition}
-                        </p>
-                        <p className="small ps-3">
-                            {definition.example}
-                        </p>
-                    </div>
-                )
-            })}
-        </div>
-    )
+  console.log(props.meaning);
+  return (
+    <div className="Meaning">
+      <h3 className="fs-5 fw-bold">{props.meaning.partOfSpeech}</h3>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index} className="mb-0">
+            <p className="mb-0 my-auto">
+            <strong className="mb-0">- </strong> {definition.definition}
+            <br/>
+            <em className="small ps-3 mt-0">{definition.example}</em>
+            <Synonyms synonyms={definition.synonyms} />
+          </p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
