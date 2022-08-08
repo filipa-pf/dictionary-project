550a7fe95ce78653c0b6407d72ea2e9a1444dfa0
fmf4te <anafilipa.paesdefaria@gmail.com>
2022-08-08 17:36:59 +0100

    added images (pexel API) and finished styling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,20 +2,31 @@ import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
 import "./Dictionary.css";
+import Photos from "./Photos";
 
 export default function Dicionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
   }
+function handlePexelsResponse(response){
+    console.log(response);
+    setPhotos(response.data.photos);
+}
 
   function search() {
     // documentation https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    const pexelsApiKey = "563492ad6f91700001000001d798a175a54d4144aad18d99e9f8559e";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = {"Authorization" : `Bearer ${pexelsApiKey}`}
+    axios.get(pexelsApiUrl, { headers: headers}).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -47,6 +58,7 @@ export default function Dicionary(props) {
                 </form>
               </section>
               <Results results={results} />
+              <Photos photos={photos}/>
             </div>
           </div>
         </div>
---
+++ src/Photos.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Photos.css";
+
+export default function Photos(props) {
+  if (props.photos) {
+    return (
+      <section className="Photos">
+        <div className="row">
+          {props.photos.map(function (photo, index) {
+            return (
+              <div className="col-4">
+                <a href={photo.src.original} target="_blank">
+                <img
+                  src={photo.src.landscape}
+                  key={index}
+                  className="img-fluid pb-3"
+                ></img></a>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    );
+  } else {
+    return null;
+  }
+}
